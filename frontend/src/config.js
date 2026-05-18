b5ba23feee91546646c0207b74211fb19200f1bc
Nitesh Nair <niteshnair@Niteshs-MacBook-Air.local>
2026-05-18 14:07:45 +0530

    updated nginx for ssl

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8000/"
-    : "http://reviewanalysis.uxlivinglab.org/api/"; 
+    : "https://reviewanalysis.uxlivinglab.org/api/"; 
 
 export default API_BASE_URL;
