2305454bcbd81a79826ca6781183bb0efd130dca
Nitesh Nair <niteshnair@Niteshs-MacBook-Air.local>
2026-05-18 11:42:45 +0530

    nginx fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8000/"
-    : "https://reviewanalysis.uxlivinglab.org/api"; 
+    : "http://reviewanalysis.uxlivinglab.org/api/"; 
 
 export default API_BASE_URL;
