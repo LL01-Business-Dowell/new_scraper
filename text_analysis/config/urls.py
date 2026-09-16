c08057eb547aec4d122e9a0938fd98d84be9472e
heenakhan4 <khanheena4997@gmail.com>
2026-09-16 15:38:21 +0530

    text-analysis proxy route updated

--- text_analysis/config/urls.py
+++ text_analysis/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/text-analysis/', include('TextAnalytics.urls')),
+    path('text-analysis/', include('TextAnalytics.urls')),
 ]
