9aa7e9ad7667a55485786a27b188548ef711663d
Nitesh Nair <niteshnair@Niteshs-MacBook-Air.local>
2026-09-23 09:07:23 +0530

    map url fix

--- frontend/src/ScanMapPage.jsx
+++ frontend/src/ScanMapPage.jsx
@@ -142,7 +142,7 @@ export default function ScanMapPage() {
 
       try {
         const response = await fetch(
-          `${baseUrl}api//scans/last24hours`
+          `${baseUrl}/api/scans/last24hours`
         );
 
         const result = await response.json();
